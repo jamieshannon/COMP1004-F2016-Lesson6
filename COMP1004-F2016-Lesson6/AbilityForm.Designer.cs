03a1c1c7d3bb3e7c76058e33e80db510af117a38
Christopher Shannon <200328763@student.georgianc.on.ca>
2016-10-13 16:27:20 -0400

    Add project files.

---
+++ COMP1004-F2016-Lesson6/AbilityForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace COMP1004_F2016_Lesson6
+{
+    partial class AbilityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StrengthLabel = new System.Windows.Forms.Label();
+            this.StrengthTextBox = new System.Windows.Forms.TextBox();
+            this.DexterityTextBox = new System.Windows.Forms.TextBox();
+            this.DexterityLabel = new System.Windows.Forms.Label();
+            this.ConstitutionTextBox = new System.Windows.Forms.TextBox();
+            this.ConstitutionLabel = new System.Windows.Forms.Label();
+            this.IntelligenceTextBox = new System.Windows.Forms.TextBox();
+            this.IntelligenceLabel = new System.Windows.Forms.Label();
+            this.WisdomTextBox = new System.Windows.Forms.TextBox();
+            this.WisdomLabel = new System.Windows.Forms.Label();
+            this.CharismaTextBox = new System.Windows.Forms.TextBox();
+            this.CharismaLabel = new System.Windows.Forms.Label();
+            this.RollAbilitiesButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StrengthLabel
+            // 
+            this.StrengthLabel.AutoSize = true;
+            this.StrengthLabel.Location = new System.Drawing.Point(29, 28);
+            this.StrengthLabel.Name = "StrengthLabel";
+            this.StrengthLabel.Size = new System.Drawing.Size(90, 22);
+            this.StrengthLabel.TabIndex = 0;
+            this.StrengthLabel.Text = "Strength";
+            // 
+            // StrengthTextBox
+            // 
+            this.StrengthTextBox.Location = new System.Drawing.Point(164, 25);
+            this.StrengthTextBox.Name = "StrengthTextBox";
+            this.StrengthTextBox.ReadOnly = true;
+            this.StrengthTextBox.Size = new System.Drawing.Size(76, 30);
+            this.StrengthTextBox.TabIndex = 1;
+            // 
+            // DexterityTextBox
+            // 
+            this.DexterityTextBox.Location = new System.Drawing.Point(164, 69);
+            this.DexterityTextBox.Name = "DexterityTextBox";
+            this.DexterityTextBox.ReadOnly = true;
+            this.DexterityTextBox.Size = new System.Drawing.Size(76, 30);
+            this.DexterityTextBox.TabIndex = 3;
+            // 
+            // DexterityLabel
+            // 
+            this.DexterityLabel.AutoSize = true;
+            this.DexterityLabel.Location = new System.Drawing.Point(29, 72);
+            this.DexterityLabel.Name = "DexterityLabel";
+            this.DexterityLabel.Size = new System.Drawing.Size(97, 22);
+            this.DexterityLabel.TabIndex = 2;
+            this.DexterityLabel.Text = "Dexterity";
+            // 
+            // ConstitutionTextBox
+            // 
+            this.ConstitutionTextBox.Location = new System.Drawing.Point(164, 113);
+            this.ConstitutionTextBox.Name = "ConstitutionTextBox";
+            this.ConstitutionTextBox.ReadOnly = true;
+            this.ConstitutionTextBox.Size = new System.Drawing.Size(76, 30);
+            this.ConstitutionTextBox.TabIndex = 5;
+            // 
+            // ConstitutionLabel
+            // 
+            this.ConstitutionLabel.AutoSize = true;
+            this.ConstitutionLabel.Location = new System.Drawing.Point(29, 116);
+            this.ConstitutionLabel.Name = "ConstitutionLabel";
+            this.ConstitutionLabel.Size = new System.Drawing.Size(129, 22);
+            this.ConstitutionLabel.TabIndex = 4;
+            this.ConstitutionLabel.Text = "Constitution";
+            // 
+            // IntelligenceTextBox
+            // 
+            this.IntelligenceTextBox.Location = new System.Drawing.Point(164, 157);
+            this.IntelligenceTextBox.Name = "IntelligenceTextBox";
+            this.IntelligenceTextBox.ReadOnly = true;
+            this.IntelligenceTextBox.Size = new System.Drawing.Size(76, 30);
+            this.IntelligenceTextBox.TabIndex = 7;
+            // 
+            // IntelligenceLabel
+            // 
+            this.IntelligenceLabel.AutoSize = true;
+            this.IntelligenceLabel.Location = new System.Drawing.Point(29, 160);
+            this.IntelligenceLabel.Name = "IntelligenceLabel";
+            this.IntelligenceLabel.Size = new System.Drawing.Size(118, 22);
+            this.IntelligenceLabel.TabIndex = 6;
+            this.IntelligenceLabel.Text = "Intelligence";
+            // 
+            // WisdomTextBox
+            // 
+            this.WisdomTextBox.Location = new System.Drawing.Point(164, 201);
+            this.WisdomTextBox.Name = "WisdomTextBox";
+            this.WisdomTextBox.ReadOnly = true;
+            this.WisdomTextBox.Size = new System.Drawing.Size(76, 30);
+            this.WisdomTextBox.TabIndex = 9;
+            // 
+            // WisdomLabel
+            // 
+            this.WisdomLabel.AutoSize = true;
+            this.WisdomLabel.Location = new System.Drawing.Point(29, 204);
+            this.WisdomLabel.Name = "WisdomLabel";
+            this.WisdomLabel.Size = new System.Drawing.Size(83, 22);
+            this.WisdomLabel.TabIndex = 8;
+            this.WisdomLabel.Text = "Wisdom";
+            // 
+            // CharismaTextBox
+            // 
+            this.CharismaTextBox.Location = new System.Drawing.Point(164, 245);
+            this.CharismaTextBox.Name = "CharismaTextBox";
+            this.CharismaTextBox.ReadOnly = true;
+            this.CharismaTextBox.Size = new System.Drawing.Size(76, 30);
+            this.CharismaTextBox.TabIndex = 11;
+            // 
+            // CharismaLabel
+            // 
+            this.CharismaLabel.AutoSize = true;
+            this.CharismaLabel.Location = new System.Drawing.Point(29, 248);
+            this.CharismaLabel.Name = "CharismaLabel";
+            this.CharismaLabel.Size = new System.Drawing.Size(97, 22);
+            this.CharismaLabel.TabIndex = 10;
+            this.CharismaLabel.Text = "Charisma";
+            // 
+            // RollAbilitiesButton
+            // 
+            this.RollAbilitiesButton.Cursor = System.Windows.Forms.Cursors.Help;
+            this.RollAbilitiesButton.Location = new System.Drawing.Point(64, 307);
+            this.RollAbilitiesButton.Name = "RollAbilitiesButton";
+            this.RollAbilitiesButton.Size = new System.Drawing.Size(140, 37);
+            this.RollAbilitiesButton.TabIndex = 12;
+            this.RollAbilitiesButton.Text = "Roll Abilities";
+            this.RollAbilitiesButton.UseVisualStyleBackColor = true;
+            this.RollAbilitiesButton.Click += new System.EventHandler(this.RollAbilitiesButton_Click);
+            // 
+            // AbilityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 379);
+            this.Controls.Add(this.RollAbilitiesButton);
+            this.Controls.Add(this.CharismaTextBox);
+            this.Controls.Add(this.CharismaLabel);
+            this.Controls.Add(this.WisdomTextBox);
+            this.Controls.Add(this.WisdomLabel);
+            this.Controls.Add(this.IntelligenceTextBox);
+            this.Controls.Add(this.IntelligenceLabel);
+            this.Controls.Add(this.ConstitutionTextBox);
+            this.Controls.Add(this.ConstitutionLabel);
+            this.Controls.Add(this.DexterityTextBox);
+            this.Controls.Add(this.DexterityLabel);
+            this.Controls.Add(this.StrengthTextBox);
+            this.Controls.Add(this.StrengthLabel);
+            this.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "AbilityForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StrengthLabel;
+        private System.Windows.Forms.TextBox StrengthTextBox;
+        private System.Windows.Forms.TextBox DexterityTextBox;
+        private System.Windows.Forms.Label DexterityLabel;
+        private System.Windows.Forms.TextBox ConstitutionTextBox;
+        private System.Windows.Forms.Label ConstitutionLabel;
+        private System.Windows.Forms.TextBox IntelligenceTextBox;
+        private System.Windows.Forms.Label IntelligenceLabel;
+        private System.Windows.Forms.TextBox WisdomTextBox;
+        private System.Windows.Forms.Label WisdomLabel;
+        private System.Windows.Forms.TextBox CharismaTextBox;
+        private System.Windows.Forms.Label CharismaLabel;
+        private System.Windows.Forms.Button RollAbilitiesButton;
+    }
+}
+
